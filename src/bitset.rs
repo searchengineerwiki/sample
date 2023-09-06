f1b48c962870e3238e3469fb56c0bd5daee7ab51
se <searchengineerwiki@gmail.com>
2023-09-06 22:28:50 +1000

    Implemented operator traits for Bitset

--- src/bitset.rs
+++ src/bitset.rs
@@ -1,3 +1,6 @@
+use core::convert::From;
+use core::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not, Sub, SubAssign};
+
 // Simple bitset, limited to values between 0 and 127
 struct Bitset {
     data: u128,
@@ -7,6 +10,11 @@ impl Bitset {
     // Creates a new Bitset
     fn new() -> Self {
         Self { data: 0 }
+    }
+
+    // Resets the bitmap back to an empty state
+    fn clear(&mut self) {
+        self.data = 0;
     }
 
     // Returns `true` if this set contains the specified value
@@ -28,14 +36,81 @@ impl Bitset {
         Ok(exists)
     }
 
-    fn clear(&mut self) {
-        self.data = 0;
+    // Returns `true` if the bitset is empty, otherwise `false`
+    fn is_empty(&self) -> bool {
+        self.data == 0
+    }
+}
+
+impl From<u128> for Bitset {
+    fn from(value: u128) -> Self {
+        Self { data: value }
+    }
+}
+
+impl BitAnd for Bitset {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        let raw = self.data & rhs.data;
+        raw.into()
+    }
+}
+
+impl BitAndAssign for Bitset {
+    fn bitand_assign(&mut self, rhs: Self) {
+        let raw = self.data & rhs.data;
+        *self = raw.into();
+    }
+}
+
+impl BitOr for Bitset {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        let raw = self.data | rhs.data;
+        raw.into()
+    }
+}
+
+impl BitOrAssign for Bitset {
+    fn bitor_assign(&mut self, rhs: Self) {
+        let raw = self.data | rhs.data;
+        *self = raw.into()
+    }
+}
+
+impl Sub for Bitset {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let raw = self.data & !rhs.data;
+        raw.into()
+    }
+}
+
+impl SubAssign for Bitset {
+    fn sub_assign(&mut self, rhs: Self) {
+        let raw = self.data & !rhs.data;
+        *self = raw.into()
+    }
+}
+
+impl Not for Bitset {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        let raw = !self.data;
+        raw.into()
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    const SET_A: Bitset = Bitset { data: 30 }; // Binary 11110, 1, 2, 3, and 4 are set
+    const SET_B: Bitset = Bitset { data: 6 }; //  Binary 00110, 1 and 2 are set
 
     #[test]
     fn test_clear() {
@@ -44,6 +119,15 @@ mod test {
         assert_eq!(true, bitset.contains(100));
         bitset.clear();
         assert_eq!(false, bitset.contains(100));
+    }
+
+    #[test]
+    fn test_from_u128() {
+        let raw: u128 = 10; // 1010 in binary
+        let bitset: Bitset = raw.into();
+
+        assert!(bitset.contains(1));
+        assert!(bitset.contains(3));
     }
 
     #[test]
@@ -70,5 +154,102 @@ mod test {
         let mut bitset = Bitset::new();
         let result = bitset.insert(128);
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut bitset = Bitset::new();
+        assert!(bitset.is_empty());
+        let _ = bitset.insert(10);
+        assert_eq!(false, bitset.is_empty());
+    }
+
+    #[test]
+    fn test_set_difference() {
+        let result = SET_A - SET_B;
+        assert!(!result.contains(1));
+        assert!(!result.contains(2));
+        assert!(result.contains(3));
+        assert!(result.contains(4));
+    }
+
+    #[test]
+    fn test_set_difference_assign() {
+        // Need a mutable set b to do assignment
+        let mut set_a = Bitset::new();
+        let _ = set_a.insert(1);
+        let _ = set_a.insert(2);
+        let _ = set_a.insert(3);
+        let _ = set_a.insert(4);
+
+        set_a -= SET_B;
+        assert!(!set_a.contains(1));
+        assert!(!set_a.contains(2));
+        assert!(set_a.contains(3));
+        assert!(set_a.contains(4));
+    }
+
+    #[test]
+    fn test_set_intersection() {
+        let result = SET_A & SET_B;
+        assert!(result.contains(1));
+        assert!(result.contains(2));
+        assert!(!result.contains(3));
+        assert!(!result.contains(4));
+    }
+
+    #[test]
+    fn test_set_intersection_assign() {
+        // Need a mutable set b to do assignment
+        let mut set_a = Bitset::new();
+        let _ = set_a.insert(1);
+        let _ = set_a.insert(2);
+        let _ = set_a.insert(3);
+        let _ = set_a.insert(4);
+
+        set_a &= SET_B;
+        assert!(set_a.contains(1));
+        assert!(set_a.contains(2));
+        assert!(!set_a.contains(3));
+        assert!(!set_a.contains(4));
+    }
+
+    #[test]
+    fn test_set_negation() {
+        let result = !SET_A;
+
+        assert!(!result.contains(1));
+        assert!(!result.contains(2));
+        assert!(!result.contains(3));
+        assert!(!result.contains(4));
+
+        assert!(result.contains(5));
+        assert!(result.contains(28));
+        assert!(result.contains(56));
+        assert!(result.contains(100));
+    }
+
+    #[test]
+    fn test_set_union() {
+        let result = SET_A | SET_B;
+        assert!(result.contains(1));
+        assert!(result.contains(2));
+        assert!(result.contains(3));
+        assert!(result.contains(4));
+    }
+
+    #[test]
+    fn test_set_union_assign() {
+        let mut set_a = Bitset::new();
+        let _ = set_a.insert(1);
+        let _ = set_a.insert(2);
+        let _ = set_a.insert(3);
+        let _ = set_a.insert(4);
+
+        set_a |= SET_B;
+        assert!(set_a.contains(1));
+        assert!(set_a.contains(2));
+        assert!(set_a.contains(3));
+        assert!(set_a.contains(4));
     }
 }
